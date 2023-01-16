d2f50ac94aefcead7254531211f9ae82f0c22fd7
Roméo VANDOOLAEGHE <romeo.vandoolaeghe@student.junia.com>
2023-01-16 09:39:38 +0100

    partie2

--- partie2.cpp
+++ partie2.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 #include <iostream>
 #include <stdlib.h>   
-#include <time.h>   
-constexpr int TAILLEVEC = 20;
-constexpr int value = 254;
+#include <time.h>  
+//partie 3 : constantes
+constexpr int TAILLEVEC = 100;
+constexpr int value = 42;
 constexpr int max_value = 100;
 
 
@@ -14,14 +15,16 @@ using namespace std;
 
 
 std::vector<int> tab;
+
 int recherchedicho(vector<int>& t, int(search_value), int(vector_size));
+void binarysearch(vector<int>& t);
 
 
 int main() {
+	//partie2 : generation d'un tableau trié de nombres aléatoires
 	srand(time(NULL));
 	for (int i = 0; i <= TAILLEVEC; i++) {
 		tab.push_back(rand() % max_value);
-
 	}
 	std::sort(tab.begin(), tab.end());
 	for (int i = 0; i <= TAILLEVEC; i++) {
@@ -30,7 +33,10 @@ int main() {
 	}
 	cout << " " << endl;
 
+
+	//test fonctions
 	cout << recherchedicho(tab, value, TAILLEVEC) << endl;
+	binarysearch(tab);
 }
 
 
@@ -56,3 +62,30 @@ int recherchedicho(vector<int>& t, int search_value, int vector_size) {
 	}
 }
 
+
+
+
+//Partie 5 : binary_search STL
+void binarysearch(vector<int>& t) {
+
+	srand(time(NULL));
+	for (int i = 0; i <= TAILLEVEC; i++) {
+		t.push_back(rand() % max_value);
+	}
+	std::sort(t.begin(), t.end());
+	for (int i = 0; i <= TAILLEVEC; i++) {
+		cout << t[i] << " ";
+
+	}
+	cout << " " << endl;
+
+	std::cout << "Searching for " << value << '\n';
+	if (std::binary_search(t.begin(), t.end(), value)) {
+		std::cout << "valeur trouvee " << '\n';
+	}
+	else {
+		std::cout << "valeur non trouvee\n";
+	}
+
+
+}
